afbf3d5415fd1d2fdfc116955ecf337a7d263180
Leigh Johnson <hi@leighjohnson.me>
2018-10-30 14:23:51 -0700

    reorganize directory structure to work with gcloud ml engines packager

--- utils/submit_training.sh
+++ utils/submit_training.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+
+now=$(date +"%Y%m%d_%H%M%S") 
+JOB_NAME="rpivision_$now"
+JOB_ID="rpivision_$now"
 gcloud ml-engine jobs submit training $JOB_NAME \
     --staging-bucket $PACKAGE_STAGING_PATH \
     --job-dir $JOB_DIR  \
     --package-path $TRAINER_PACKAGE_PATH \
     --module-name $MAIN_TRAINER_MODULE \
     --region $REGION \
+    --python-version 3.5 \
+    --runtime-version 1.10
