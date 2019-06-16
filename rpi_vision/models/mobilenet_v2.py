a922f8f6230042ca6c695d1e84c9d2c89dfa9ad0
Leigh Johnson <hi@leighjohnson.me>
2019-06-16 16:35:37 -0700

    fix improts

--- rpi_vision/models/mobilenet_v2.py
+++ rpi_vision/models/mobilenet_v2.py
@@ -2,8 +2,8 @@
 import logging
 # lib
 import tensorflow as tf
-from tensorflow.keras.applications.mobilenetv2 import MobileNetV2
-from tensorflow.keras.applications.mobilenetv2 import preprocess_input, decode_predictions
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 
 logging.basicConfig()
 
