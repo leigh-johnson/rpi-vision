d21ea2683a1b30384600e092b46bbbb6f6ab200e
Leigh Johnson <hi@leighjohnson.me>
2019-06-27 12:21:10 -0700

    add --tflite flag to mobilenet_v2 agent

--- rpi_vision/agent/mobilenet_v2.py
+++ rpi_vision/agent/mobilenet_v2.py
@@ -15,22 +15,32 @@ def parse_args():
                         dest='include_top', default=True,
                         help='Include fully-connected layer at the top of the network.')
 
+    parser.add_argument('--tflite', type=bool,
+                        dest='tflite', action='store_true', default=False,
+                        help='Convert base model to TFLite FlatBuffer, then load model into TFLite Python Interpreter')
     args = parser.parse_args()
     return args
 
 
+def main(args):
+    args_dict = args.to_dict()
+    model = MobileNetV2Base(**args_dict)
+    capture_manager = PiCameraStream()
+    capture_manager.start()
+
+    while not capture_manager.stopped:
+        if capture_manager.frame is not None:
+            frame = capture_manager.read()
+            if args.tflite:
+                prediction = model.tflite_predict(frame)
+            else:
+                prediction = model.predict(frame)
+            logging.info(prediction)
+
+
 if __name__ == "__main__":
     args = parse_args()
-    args_dict = args.to_dict()
     try:
-        model = MobileNetV2Base(**args_dict)
-        capture_manager = PiCameraStream()
-        capture_manager.start()
-
-        while not capture_manager.stopped:
-            if capture_manager.frame is not None:
-                frame = capture_manager.read()
-                prediction = model.predict(frame)
-                logging.info(prediction)
+        main(args)
     except KeyboardInterrupt:
         capture_manager.stop()
